9dee930f15f8e9b2d5ccd33c7f4522d28106821b
ali ramezani <66328048+neveralone0@users.noreply.github.com>
2022-08-27 18:04:37 +0430

    Update settings.py

--- A/A/settings.py
+++ A/A/settings.py
@@ -137,7 +137,7 @@ AUTHENTICATION_BACKENDS = [
 # google account
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'neveralone720@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = 'wvlbydkqzctmquqq'
 EMAIL_USE_TLS = True
