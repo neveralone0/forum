8ceb4d69d3c673dd7fbb5142050bdb773aafd9de
ali ramezani <66328048+neveralone0@users.noreply.github.com>
2022-08-27 18:52:29 +0430

    Update settings.py

--- A/A/settings.py
+++ A/A/settings.py
@@ -139,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'wvlbydkqzctmquqq'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'our stack email system'
 
